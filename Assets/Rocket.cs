247b7b37b9f6418f8d469ea167fa76bbc5e8b700
Rob <rob.hartley@gmail.com>
2021-01-14 11:13:24 +1000

    Basic Rocket Audio Added

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,11 +7,14 @@ public class Rocket : MonoBehaviour {
 
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start() {
 
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -30,6 +33,12 @@ public class Rocket : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.Space)) {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+        } else {
+            audioSource.Stop();
+        
         }
 
     } // ProcessInput()
