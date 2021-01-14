1a7f13e9802fa9daa5a4da596b75b733ef79df0e
Rob <rob.hartley@gmail.com>
2021-01-14 17:14:10 +1000

    53

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 175f;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
@@ -17,31 +19,61 @@ public class Rocket : MonoBehaviour {
 
     }
 
+
     // Update is called once per frame
     void Update() {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput() {
-        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
-            transform.Rotate(Vector3.forward);
-        }
 
-        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) {
-            transform.Rotate(-Vector3.forward);
-        }
+    private void Thrust() {
+
+        float thrustThisFrame = mainThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!audioSource.isPlaying) {
                 audioSource.Play();
             }
         } else {
             audioSource.Stop();
+
+        }
+    }
+
+
+    private void Rotate() {
         
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        //rigidBody.freezeRotation = true;
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
+            //transform.Rotate(Vector3.forward * rotationThisFrame);
+            rigidBody.AddRelativeTorque(Vector3.forward * rotationThisFrame);
         }
 
-    } // ProcessInput()
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) {
+            //transform.Rotate(-Vector3.forward * rotationThisFrame);
+            rigidBody.AddRelativeTorque(-Vector3.forward * rotationThisFrame);
+        }
 
+        //rigidBody.freezeRotation = false;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        
+        switch(collision.gameObject.tag) {
+
+            case "Friendly":
+                print("HAPPY"); // TODO :: Change this one to pickups and fuel
+                break;
+            
+            default:
+                print("DEATH"); 
+                break;
+        }
+    }
 
 }
